bb6db2501ecc046c236ddb6c0bc94662d2bffc9a
wushijie <wushijie@melot.cn>
2023-11-01 19:05:57 +0800

    clock

--- clock/index.js
+++ clock/index.js
@@ -1,22 +1,21 @@
 $(function ($) {
+    /** @type {HTMLCanvasElement} */
     const canvas = $('#canvas')[0]
     const ctx = canvas.getContext('2d')
 
-    const clock = () => {
-        ctx.clearRect(0, 0, canvas.width, canvas.height)
+    // 画布的宽高
+    const cs = 160
+    canvas.width = canvas.height = cs
+    // 当前时分秒
+    const now = new Date()
+    const cuh = now.getHours() >= 12 ? (now.getHours() - 12) : now.getHours()
+    const cum = now.getMinutes()
+    const cus = now.getSeconds()
 
-        ctx.translate(canvas.width / 2, canvas.height / 2)
-        ctx.save()
-        ctx.strokeStyle = '#325FA2'
-        ctx.lineWidth = 5
-        
-        ctx.beginPath()
-        ctx.arc(0, 0, 120, 0, Math.PI * 2)
-        ctx.stroke()
-        
-        ctx.restore()
+    ctx.fillStyle = 'pink'
+    ctx.fillRect(55, 55, 50, 50)
 
-    }
-
-    clock()
+    ctx.fillStyle = 'blue'
+    ctx.rotate(Math.PI / 6)
+    ctx.fillRect(55, 55, 50, 50)
 })
