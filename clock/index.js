e1b491b6398e6eaef7fb709c7e4c6f7fb32fb436
guozi007a <1208001055@qq.com>
2023-11-02 01:36:45 +0800

    clock

--- clock/index.js
+++ clock/index.js
@@ -3,19 +3,14 @@ $(function ($) {
     const canvas = $('#canvas')[0]
     const ctx = canvas.getContext('2d')
 
-    // 画布的宽高
-    const cs = 160
-    canvas.width = canvas.height = cs
-    // 当前时分秒
-    const now = new Date()
-    const cuh = now.getHours() >= 12 ? (now.getHours() - 12) : now.getHours()
-    const cum = now.getMinutes()
-    const cus = now.getSeconds()
+    canvas.width = canvas.height = 500
 
-    ctx.fillStyle = 'pink'
-    ctx.fillRect(55, 55, 50, 50)
-
-    ctx.fillStyle = 'blue'
+    ctx.translate(100, 100)
+    ctx.lineWidth = 4
+    ctx.lineCap = 'round'
+    ctx.beginPath()
+    ctx.moveTo(0, 0)
     ctx.rotate(Math.PI / 6)
-    ctx.fillRect(55, 55, 50, 50)
+    ctx.lineTo(0, -20)
+    ctx.stroke()
 })
